5b00e4bda13840b07f30a41a8e1591dcb8ba995f
seojin kim <001106ksj@gmail.com>
2021-12-28 13:50:20 +0900

    feat: 전시 소개 페이지 반응형 (진행 중)

--- src/pages/about/small/index.tsx
+++ src/pages/about/small/index.tsx
@@ -1,34 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+import AboutBottomLeft from '../../../components/About/BottomLeft';
+import BottomMent from '../../../components/About/BottomMent';
+import StudentsByClass from '../../../components/About/Graduates';
+import TopMent from '../../../components/About/TopMent';
 import { BgAboutTitleSmall } from '../../../components/common/Icons';
 
-const Wrapper = styled.div``;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100vh;
-  position: absolute;
-  top: 0;
-  left: 0;
-  object-fit: cover;
-  z-index: 0;
+const Wrapper = styled.div`
+  flex-direction: column;
+  display: flex;
 `;
 
-const FlexWrapper = styled.div`
+const MentWrapper = styled.div`
+  font-family: Noto Sans Regular;
+  font-size: 10px;
+  line-height: 16px;
+  letter-spacing: -0.05em;
+  & p {
+    margin-bottom: 8px;
+  }
+`;
+const TitleWrapper = styled.div`
   margin-top: 200px;
-  background-color: white;
-  border-top-left-radius: 30px;
-  border-top-right-radius: 30px;
-  z-index: 20;
+  margin-bottom: 40px;
+  padding: 0 20px;
+  padding-bottom: 30px;
+  border-bottom: 1px solid #2454a6;
 `;
 
 function AboutSmall() {
   return (
     <Wrapper>
-      <FlexWrapper>
+      <TitleWrapper>
         <BgAboutTitleSmall />
-      </FlexWrapper>
-      <Image src="/statics/bg-main.webp" alt="201512119_김미연" />
+      </TitleWrapper>
+      <MentWrapper>
+        <TopMent />
+      </MentWrapper>
+      <MentWrapper>
+        <BottomMent />
+      </MentWrapper>
+      <StudentsByClass />
+      <AboutBottomLeft />
     </Wrapper>
   );
 }
