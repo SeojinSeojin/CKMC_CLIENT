b108a7c9164534d946a0e03636c60a24f7353d6a
seojin kim <001106ksj@gmail.com>
2021-12-29 09:08:40 +0900

    fix: react-responsive-carousel이 페이지가 넘어갔을 때 초기화되지 않는 이슈 수정

--- src/components/Episode/PageViewer/index.tsx
+++ src/components/Episode/PageViewer/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import { CustomCarousel, Item } from './style';
 
 function PageViewer({ pages }: { pages: Array<string> }) {
+  const [selectedPage, setSelectedPage] = useState(0);
+  useEffect(() => {
+    setSelectedPage(0);
+  }, [pages]);
+
   return (
     <CustomCarousel
       showArrows={true}
@@ -11,6 +16,8 @@ function PageViewer({ pages }: { pages: Array<string> }) {
       showIndicators={false}
       emulateTouch={true}
       showThumbs={false}
+      selectedItem={selectedPage}
+      onChange={(e) => setSelectedPage(e)}
     >
       {pages.map((page, idx) => (
         <Item key={idx}>
