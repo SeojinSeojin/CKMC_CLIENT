4944bae4f3520db5db9ccd3bd8e98cba168aff86
seojin kim <001106ksj@gmail.com>
2022-01-01 20:43:02 +0900

    fix: 작가 작품 페이지 윗 마진 수정

--- src/components/layout/Author/index.tsx
+++ src/components/layout/Author/index.tsx
@@ -5,7 +5,7 @@ import useResponsive from '../../../hooks/useResponsive';
 function AuthorLayout({ children }: { children: React.ReactNode }) {
   const { isSmall, isSmallMiddle, isBigMiddle } = useResponsive();
   return (
-    <FlexWrapper>
+    <FlexWrapper isSmall={isSmall || isSmallMiddle} isBigMiddle={isBigMiddle}>
       <GridWrapper isSmall={isSmall || isSmallMiddle} isBigMiddle={isBigMiddle}>
         {children}
       </GridWrapper>
@@ -13,28 +13,25 @@ function AuthorLayout({ children }: { children: React.ReactNode }) {
   );
 }
 
-interface IGridWrapper {
+interface IIsSmall {
   isSmall: boolean;
   isBigMiddle: boolean;
 }
-const GridWrapper = styled.div<IGridWrapper>`
+const GridWrapper = styled.div<IIsSmall>`
   display: grid;
   grid-template-columns: ${({ isSmall }) => (isSmall ? '1fr' : '500px auto')};
   width: ${({ isBigMiddle }) => (isBigMiddle ? '94%' : '80%')};
   gap: ${({ isSmall }) => (isSmall ? '20px' : '80px')};
   ${({ isSmall }) =>
-    isSmall
-      ? 'margin-top: 85px;'
-      : '& > div:nth-child(2) { max-height: 88vh; overflow-y: scroll; }'}
+    isSmall ? '' : '& > div:nth-child(2) { max-height: 88vh; overflow-y: scroll; }'}
 `;
 
-const FlexWrapper = styled.div`
+const FlexWrapper = styled.div<IIsSmall>`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 88vh;
-  margin-top: 6vh;
-  margin-bottom: 6vh;
+  ${({ isSmall }) =>
+    isSmall ? 'margin-top: 43px;' : 'min-height: 88vh;margin-top: 6vh;margin-bottom: 6vh;'}
 `;
 
 export default AuthorLayout;
