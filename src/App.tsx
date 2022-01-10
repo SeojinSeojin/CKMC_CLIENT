4608d06da88090f93d01431157fd4e43c84d8a9b
seojin kim <001106ksj@gmail.com>
2022-01-10 14:04:54 +0900

    fix: lazy loading 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { Redirect, Route, Switch, useLocation } from 'react-router';
 import ReactGA from 'react-ga';
 import { useUser } from './hooks/useUser';
-import AboutPage from './pages/about';
-import MapPage from './pages/map';
-import AuthorPage from './pages/author';
-import EpisodePage from './pages/episode';
-import GuestPage from './pages/guest';
-import LoginPage from './pages/login';
-import MyPage from './pages/mypage';
-import Upload from './pages/mypage/write';
-import WorksPage from './pages/works';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAnalytics } from './hooks/useAnalytics';
 import Home from './pages/home';
-import EventPage from './pages/event';
+
+const EpisodePage = React.lazy(() => import('./pages/episode'));
+const EventPage = React.lazy(() => import('./pages/event'));
+const AboutPage = React.lazy(() => import('./pages/about'));
+const MapPage = React.lazy(() => import('./pages/map'));
+const AuthorPage = React.lazy(() => import('./pages/author'));
+const GuestPage = React.lazy(() => import('./pages/guest'));
+const LoginPage = React.lazy(() => import('./pages/login'));
+const MyPage = React.lazy(() => import('./pages/mypage'));
+const Upload = React.lazy(() => import('./pages/mypage/write'));
+const WorksPage = React.lazy(() => import('./pages/works'));
 
 function App() {
   const { isValidating, author } = useUser();
@@ -31,26 +32,28 @@ function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/about/map" component={MapPage} />
-        <Route exact path="/author/:authorId" component={AuthorPage} />
-        <Route exact path="/author/:authorId/:episodeIdx" component={EpisodePage} />
-        <Route exact path="/guest" component={GuestPage} />
-        <Route exact path="/event" component={EventPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/mypage">
-          {!isValidating && !author ? <Redirect to="/" /> : <MyPage />}
-        </Route>
-        <Route exact path="/mypage/write" component={Upload}>
-          {!isValidating && !author ? <Redirect to="/" /> : <Upload isUpload={true} />}
-        </Route>
-        <Route exact path="/mypage/edit/:episodeIdx" component={Upload}>
-          {!isValidating && !author ? <Redirect to="/" /> : <Upload isUpload={false} />}
-        </Route>
-        <Route exact path="/works" component={WorksPage} />
-      </Switch>
+      <Suspense fallback={<></>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/about/map" component={MapPage} />
+          <Route exact path="/author/:authorId" component={AuthorPage} />
+          <Route exact path="/author/:authorId/:episodeIdx" component={EpisodePage} />
+          <Route exact path="/guest" component={GuestPage} />
+          <Route exact path="/event" component={EventPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/mypage">
+            {!isValidating && !author ? <Redirect to="/" /> : <MyPage />}
+          </Route>
+          <Route exact path="/mypage/write" component={Upload}>
+            {!isValidating && !author ? <Redirect to="/" /> : <Upload isUpload={true} />}
+          </Route>
+          <Route exact path="/mypage/edit/:episodeIdx" component={Upload}>
+            {!isValidating && !author ? <Redirect to="/" /> : <Upload isUpload={false} />}
+          </Route>
+          <Route exact path="/works" component={WorksPage} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
